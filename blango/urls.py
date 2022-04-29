2111991faac7e03a11d70b288a032ac784f62315
Shubham Verma <20shubh01@gmail.com>
2022-04-29 17:55:39 +0000

    Finish django registration

--- blango/urls.py
+++ blango/urls.py
@@ -1,4 +1,9 @@
 import blog.views
+
+import blango_auth.views
+
+from django_registration.backends.activation.views import RegistrationView
+from blango_auth.forms import BlangoRegistrationForm
 """blango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -26,7 +31,17 @@ urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     
-    path("ip/", blog.views.get_ip)
+    path("ip/", blog.views.get_ip),
+
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", blango_auth.views.profile, name="profile"),
+
+    path(
+    "accounts/register/",
+    RegistrationView.as_view(form_class=BlangoRegistrationForm),
+    name="django_registration_register",
+),
+path("accounts/", include("django_registration.backends.activation.urls")),
 ]
 
 # from django.conf import settings
